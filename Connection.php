8e7f8baeef13936c03425356a50a50d179fd5682
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-03-07 17:02:34 +0200

    Add files via upload

---
+++ Connection.php
@@ -0,0 +1,24 @@
+<?php
+
+	require_once("connection.php");
+	
+	if(isset($_POST['submit'])){
+	
+	$USER_ID = $POST['USER_ID'];
+	$salt = "jdfhsfuhsfiuh4353453543535";
+	$PASSWORD = $_POST['PASSWORD'].$salt;
+	$PASSWORD = shal($PASSWORD);
+	if(mysqli_query($connection,"INSERT INTO 'USERS'(
+		'USER_ID',
+		'PASSWORD',
+		 ) VALUES (
+		 '{$USER_ID}',
+		 '{$PASSWORD}');")){
+//where user will go once correct info is submitted 	
+			header("Location:  somthingsomething.php ");
+			exit;
+		}
+	}
+?>
+	
+
