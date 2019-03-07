6dd3148bd246dbaa6bec0abf6bad7ba21f931cf5
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-03-07 17:06:31 +0200

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -8,10 +8,8 @@
 	$PASSWORD = $_POST['PASSWORD'].$salt;
 	$PASSWORD = shal($PASSWORD);
 	if(mysqli_query($connection,"INSERT INTO 'USERS'(
-		'USER_ID',
 		'PASSWORD',
 		 ) VALUES (
-		 '{$USER_ID}',
 		 '{$PASSWORD}');")){
 //where user will go once correct info is submitted 	
 			header("Location:  somthingsomething.php ");
