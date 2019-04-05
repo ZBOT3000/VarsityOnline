9e1f07b80b418219b7283991a4859e7f0349aa8e
Lungilemngo <45350946+Lungilemngo@users.noreply.github.com>
2019-04-05 09:06:37 +0200

    errors fixed

---
+++ Academics.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_POST['email'])
+{
+  $schoolName = $_POST["School Name"];
+  $surname=$_POST["Current Grade"];
+  $schoolName = $_POST["HL"];
+  $surname=$_POST["FAL"];
+  $schoolName = $_POST["Mathematics"];
+  $surname=$_POST["Subject1"];
+  $schoolName = $_POST["Subject2"];
+  $surname=$_POST["Subject3"];
+  $surname=$_POST["Subject3"];
+}
+
+ ?>
