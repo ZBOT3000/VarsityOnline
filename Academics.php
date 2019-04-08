14cebcb8fc3e69d08a05f65f26fefdbc564859b2
Lungilemngo <45350946+Lungilemngo@users.noreply.github.com>
2019-04-08 13:10:16 +0200

    Update Academics.php

--- Academics.php
+++ Academics.php
@@ -18,20 +18,22 @@
   $LOMark = $_POST["LOMark"];
 
 
-  $mysql_qry="insert INTO `current_academics` (`CA_ID`, `USER_ID`, `SCHOOL_NAME`, `CURRENT_GRADE`, `HOME_LAN`, `HOME_LAN_MARK`, `FA_LAN`, `FA_LAN_MARK`, `MATH_SUBJ`, `MATH_SUBJ_MARK`, `SUBJ1`, `SUBJ1_MARK`, `SUBJ2`, `SUBJ2_MARK`, `SUBJ3`, `SUBJ3_MARK`, `LO_MARK`) VALUES (NULL, '25', '$schoolName', '$grade', '$HL', '$HLMark', '$FAL', '$FALMark', '$Mathematics', '$MathMark', '$Subject1', '$sub1Mark', '$Subject2', '$sub2Mark', '$Subject3', '$sub3Mark', '$LOMark')";
+  $mysql_qry="insert into CURRENT_ACADEMICS (CA_ID, USER_ID, SCHOOL_NAME, CURRENT_GRADE, HOME_LAN, HOME_LAN_MARK, FA_LAN, FA_LAN_MARK, MATH_SUBJ,
+   MATH_SUBJ_MARK, SUBJ1, SUBJ1_MARK, SUBJ2, SUBJ2_MARK,SUBJ3, SUBJ3_MARK, LO_MARK) values
+   (NULL, '25', '$schoolName', '$grade', '$HL', '$HLMark', '$FAL', '$FALMark', '$Mathematics', '$MathMark', '$Subject1',
+   '$sub1Mark', '$Subject2', '$sub2Mark', '$Subject3', '$sub3Mark', '$LOMark');";
 
 
    $result = mysqli_query($conn,$mysql_qry);
   if  ($result)
   {
   	 echo "YOU HAVE SUCCESSFULY CAPTURED YOUR ACADEMICS <br>";
-
      echo "<p><a href='http://vor.lbmsolutions.co.za/Demographics.html'>CLICK HERE TO GO BACK</a></p>";
   }
   else
   {
     	echo "SOME OF THE INFOMATION ENTERED WAS INCORRECT PLEASE RETRY AGE";
-      echo "<p><a href='http://vor.lbmsolutions.co.za/Academics.html'>CLICK HERE TO GO BACK</a></p>"; 
+      echo "<p><a href='http://vor.lbmsolutions.co.za/Academics.html'>CLICK HERE TO GO BACK</a></p>";
   }
 
  ?>
