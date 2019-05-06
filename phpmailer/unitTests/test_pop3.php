8159606e3bf143ba13572e88b6dbe26fccf740a4
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-05-06 14:45:46 +0200

    Update test_pop3.php

--- phpmailer/unitTests/test_pop3.php
+++ phpmailer/unitTests/test_pop3.php
@@ -5,7 +5,7 @@ use PHPUnit\DbUnit\TestCaseTrait;
 class pop3Test extends TestCase{
   
  public function test_testing(){
-   require_once('/home/travis/build/noreplysims/VarsityOnline/phpmailer/unitTests/pop3.php');
+   require_once('/home/travis/build/noreplysims/VarsityOnline/phpmailer/pop3.php');
    $test=testing(10,4);
  $this->assertEquals(14,$test, "correct!"); 
  }
