30c4ad4b65fbe6f09f6524fd305d8acbb90c1ea5
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-05-29 16:00:09 +0200

    Update pop3.php

--- phpmailer/pop3.php
+++ phpmailer/pop3.php
@@ -1,7 +1,22 @@
 <?php
 
-function testing($a,$b){
-        
-        return $b;
-    }
-?>
+session_start();
+require 'conn.php';
+$username = $_POST["logUsername"];
+$password = $_POST["logPassword"];
+$mysql_qry="select * from register where username like '$username' and password like '$password' ;";
+$result = mysqli_query($conn,$mysql_qry);
+if ( mysqli_num_rows( $result ) > 0 )
+{
+	$_SESSION['username']=$username;
+	$str = $_SESSION['username'];
+	$str = preg_replace('/\D/', '', $str);
+	$_SESSION['user_id']=$str;
+	header("Location: Dashboard.php");
+}
+else
+{
+	header("Location: login.php");
+}
+
+ ?>
