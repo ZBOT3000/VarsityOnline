85559bb054d384c827826ef74a89aab0d2903f19
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-05-29 23:28:50 +0200

    Update test_pop4.php

--- phpmailer/unitTests/test_pop4.php
+++ phpmailer/unitTests/test_pop4.php
@@ -6,8 +6,8 @@ class pop4Test extends TestCase{
   
  public function test_testing(){
    require_once('/home/travis/build/noreplysims/VarsityOnline/phpmailer/pop4.php');
-   $test=testing(10,4);
- $this->assertEquals(15,$test, "correct!"); 
+   $tests=testings(10,4);
+ $this->assertEquals(15,$tests, "correct!"); 
  }
  
 }
