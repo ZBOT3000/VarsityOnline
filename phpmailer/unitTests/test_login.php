2180c2fe80c5b5f84e73df005d020dc5c96aae32
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-05-30 10:25:55 +0200

    Create test_login.php

---
+++ phpmailer/unitTests/test_login.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @coversDefaultClass \Rovholo\datalabel\login
+ */
+class loginTest extends PHPUnit_Framework_TestCase{
+  protected $result;
+  
+  public function setUp(){//this part of the code initiates the hello variable
+    $this->result = new \noreplysims\VarsityOnline\Plogin.php;
+    $link = mysqli_connect('localhost', 's1434995' , 's1434995' , 'd1434995');
+    mysqli_query($link,"CREATE TABLE users (user_id int NOT NULL,user_name varchar(255),password varchar(255))");
+    mysqli_query($link,"INSERT INTO users (user_id,user_name,password) values('1','user','name')");
+  }
+  /**
+   * @covers ::login
+   */
+  public function testinit(){//this part of the code checks if the value returned by the world() method is equal to word
+    $password = "name";
+    $this->assertTrue( json_decode( $this->result->login("user"),true )[0]['password'] == $password );
+    $this->assertTrue( count( json_decode( $this->result->login("") ) ) == 0 );
+  }
+}
+?>
