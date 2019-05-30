75759287ac6a7fa05a4ed6b672e18fc91ee07ab9
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-05-30 12:08:25 +0200

    Create test_HelloWorld.php

---
+++ phpmailer/unitTests/test_HelloWorld.php
@@ -0,0 +1,18 @@
+
+<?php
+/**
+ * 
+ */
+class HelloWorldTest extends \PHPUnit_Framework_TestCase{
+  protected $hello;
+  
+  public function setUp(){//this part of the code initiates the hello variable
+    $this->hello = new \MS35\WitsSoftwareDesign\HelloWorld();
+  }
+  /**
+   * @covers ::world
+   */
+  public function testHelloWorld(){//this part of the code checks if the value returned by the world() method is equal to word
+    $this->assertSame('world',$this->hello->world());
+  }
+}
