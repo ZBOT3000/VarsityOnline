b42188018bd9aa56b98639d6e0cb5785206b4404
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-09-14 20:23:25 +0200

    Update witsBAApp.php

--- UCT/witsBAApp.php
+++ UCT/witsBAApp.php
@@ -6,11 +6,11 @@ session_start();
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Wits Application</title>
+    <title>UCT Application</title>
     <link rel="stylesheet" type="text/css" href="witsApplication.css">
   </head>
   <body>
-    <h1>WITS APPLICATION</h1>
+    <h1>UCT APPLICATION</h1>
     <div class="LogIn-Form">
   		<form id="LogIn-Form"  method="post" action="witsComAppPHP.php">
   			<h1>FILL IN YOUR ACADEMICS INFOMATION BELOW </h1><br>
@@ -18,40 +18,32 @@ session_start();
   			<div class="Drop-Down">
   				<select name="firstChoice" class="select-box">
     				<option>Select your first Choice</option>
-            <option value="BCom-General">BCom-General</option>
-            <option value="Bcom-Accounting Science">Bcom-Accounting Science</option>
-            <option value="BCom-Accounting">Bcom-Accounting</option>
-            <option value="BCom-Economic Science">Bcom-Economic Science</option>
-            <option value="BCom-Politics,Philosophy and Economicss">Bcom-Politics,Philosophy and Economicss</option>
-            <option value="BCom-Law">Bcom-Law</option>
-            <option value="BCom-Information Systems">Bcom-Information Systems</option>
+            <option value="Business Science">Bcom-Business Science</option>
+            <option value="Accounting">Bcom-Accounting</option>
+            <option value="Actuarial Science">Bcom-Actuarial Science</option>
+            <option value="Information Systems">Bcom-Information Systems</option>
+            <option value="Economics">Bcom-Economics</option>
   		     </select>
 
   			</div>
         <div class="Drop-Down">
           <select name="secondChoice" class="select-box">
-            <option>Select your first Choice</option>
-            <option value="BCom-General">BCom-General</option>
-            <option value="Bcom-Accounting Science">Bcom-Accounting Science</option>
-            <option value="BCom-Accounting">Bcom-Accounting</option>
-            <option value="BCom-Economic Science">Bcom-Economic Science</option>
-            <option value="BCom-Politics,Philosophy and Economicss">Bcom-Politics,Philosophy and Economicss</option>
-            <option value="BCom-Law">Bcom-Law</option>
-            <option value="BCom-Information Systems">Bcom-Information Systems</option>
+            <option value="Business Science">Bcom-Business Science</option>
+            <option value="Accounting">Bcom-Accounting</option>
+            <option value="Actuarial Science">Bcom-Actuarial Science</option>
+            <option value="Information Systems">Bcom-Information Systems</option>
+            <option value="Economics">Bcom-Economics</option>
            </select>
 
         </div>
 
         <div class="Drop-Down">
           <select name="ThirdChoice" class="select-box">
-            <option>Select your first Choice</option>
-            <option value="BCom-General">BCom-General</option>
-            <option value="Bcom-Accounting Science">Bcom-Accounting Science</option>
-            <option value="BCom-Accounting">Bcom-Accounting</option>
-            <option value="BCom-Economic Science">Bcom-Economic Science</option>
-            <option value="BCom-Politics,Philosophy and Economicss">Bcom-Politics,Philosophy and Economicss</option>
-            <option value="BCom-Law">Bcom-Law</option>
-            <option value="BCom-Information Systems">Bcom-Information Systems</option>
+            <option value="Business Science">Bcom-Business Science</option>
+            <option value="Accounting">Bcom-Accounting</option>
+            <option value="Actuarial Science">Bcom-Actuarial Science</option>
+            <option value="Information Systems">Bcom-Information Systems</option>
+            <option value="Economics">Bcom-Economics</option>
            </select>
         </div>
 
