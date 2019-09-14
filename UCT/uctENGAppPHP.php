d65f7321208ead05466df2f2a6727ac6908f438f
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-09-14 21:26:11 +0200

    Update and rename witsENGAppPHP.php to uctENGAppPHP.php

--- UCT/uctENGAppPHP.php
+++ UCT/uctENGAppPHP.php
@@ -6,17 +6,17 @@ $id = (int)$_SESSION['user_id'];
 $first = $_POST["firstChoice"];
 $second = $_POST["secondChoice"];
 $third = $_POST["ThirdChoice"];
-$mysql_ins="insert into wits_application (COURSE1, COURSE2, COURSE3, USER_ID) values ('$first', '$second', '$third', $id);";
+$mysql_ins="insert into uct_application (COURSE1, COURSE2, COURSE3, USER_ID) values ('$first', '$second', '$third', $id);";
 $result = mysqli_query($conn,$mysql_ins);
 if  ($result)
 {
-   echo "YOU HAVE SUCCESSFULY CAPTURED YOUR WITS APPLICATION <br>";
+   echo "YOU HAVE SUCCESSFULY CAPTURED YOUR UCT APPLICATION <br>";
    echo "<p><a href='dashboard.php'>CLICK HERE TO GO BACK</a></p>";
    $_SESSION['finish']=$_SESSION['finish']+1;
 }
 else
 {
     echo "SOME OF THE INFOMATION ENTERED WAS INCORRECT PLEASE RETRY AGAIN";
-    echo "<p><a href='witsApplication.php'>CLICK HERE TO GO BACK</a></p>";
+    echo "<p><a href='uctApplication.php'>CLICK HERE TO GO BACK</a></p>";
 }
  ?>
