dadf41b8dec0c9c7e3f37516ce7bdc4a21ad4a08
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-09-14 20:31:20 +0200

    Update uctComAppPHP.php

--- UCT/uctComAppPHP.php
+++ UCT/uctComAppPHP.php
@@ -5,11 +5,11 @@ $id = (int)$_SESSION['user_id'];
 $first = $_POST["firstChoice"];
 $second = $_POST["secondChoice"];
 $third = $_POST["ThirdChoice"];
-$mysql_ins="insert into wits_application (COURSE1, COURSE2, COURSE3, USER_ID) values ('$first', '$second', '$third', $id);";
+$mysql_ins="insert into uct_application (COURSE1, COURSE2, COURSE3, USER_ID) values ('$first', '$second', '$third', $id);";
 $result = mysqli_query($conn,$mysql_ins);
 if  ($result)
 {
-   echo "YOU HAVE SUCCESSFULY CAPTURED YOUR WITS APPLICATION <br>";
+   echo "YOU HAVE SUCCESSFULY CAPTURED YOUR UCT APPLICATION <br>";
    echo "<p><a href='dashboard.php'>CLICK HERE TO GO BACK</a></p>";
    $_SESSION['finish']=$_SESSION['finish']+1;
 }
