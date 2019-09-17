536a89306815214898be2e7a36823e28f038cf59
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-09-17 11:25:49 +0200

    Updated

--- UCT/uctBEngApp.php
+++ UCT/uctBEngApp.php
@@ -6,11 +6,12 @@ session_start();
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Wits Application</title>
-    <link rel="stylesheet" type="text/css" href="witsApplication.css">
+    <title>UCT Application</title>
+    <link rel="stylesheet" type="text/css" href="uctBAApp.css">
   </head>
   <body>
     <h1>UCT APPLICATION</h1>
+    <h1>ENGINEERING</h1>
     <div class="LogIn-Form">
   		<form id="LogIn-Form"  method="post" action="witsComAppPHP.php">
   			<h1>FILL IN YOUR ACADEMICS INFOMATION BELOW </h1><br>
@@ -18,34 +19,34 @@ session_start();
   			<div class="Drop-Down">
   				<select name="firstChoice" class="select-box">
     				<option>Select your first Choice</option>
-            <option value="Civil Engineering">BEng-Civil Engineering</option>
-            <option value="Electrical Engineering">BEng-Electrical Engineering</option>
-            <option value="Chemical Engineering">BEng-Chemical Engineering</option>
-            <option value="Mechatronics">BEng-Machatronics</option>
-            <option value="Mechanical Engineering">BEng-Mechanical Engineering</option>
+            <option value="Civil Engineering">BEng Civil Engineering</option>
+            <option value="Electrical Engineering">BEng Electrical Engineering</option>
+            <option value="Chemical Engineering">BEng Chemical Engineering</option>
+            <option value="Mechatronics">BEng Machatronics</option>
+            <option value="Mechanical Engineering">BEng Mechanical Engineering</option>
   		     </select>
 
   			</div>
         <div class="Drop-Down">
           <select name="secondChoice" class="select-box">
-            <option>Select your first Choice</option>
-             <option value="Civil Engineering">BEng-Civil Engineering</option>
-            <option value="Electrical Engineering">BEng-Electrical Engineering</option>
-            <option value="Chemical Engineering">BEng-Chemical Engineering</option>
-            <option value="Mechatronics">BEng-Machatronics</option>
-            <option value="Mechanical Engineering">BEng-Mechanical Engineering</option>
+            <option>Select your second Choice</option>
+            <option value="Civil Engineering">BEng Civil Engineering</option>
+            <option value="Electrical Engineering">BEng Electrical Engineering</option>
+            <option value="Chemical Engineering">BEng Chemical Engineering</option>
+            <option value="Mechatronics">BEng Machatronics</option>
+            <option value="Mechanical Engineering">BEng Mechanical Engineering</option>
            </select>
 
         </div>
 
         <div class="Drop-Down">
           <select name="ThirdChoice" class="select-box">
-            <option>Select your first Choice</option>
-             <option value="Civil Engineering">BEng-Civil Engineering</option>
-            <option value="Electrical Engineering">BEng-Electrical Engineering</option>
-            <option value="Chemical Engineering">BEng-Chemical Engineering</option>
-            <option value="Mechatronics">BEng-Machatronics</option>
-            <option value="Mechanical Engineering">BEng-Mechanical Engineering</option>
+            <option>Select your third Choice</option>
+            <option value="Civil Engineering">BEng Civil Engineering</option>
+            <option value="Electrical Engineering">BEng Electrical Engineering</option>
+            <option value="Chemical Engineering">BEng Chemical Engineering</option>
+            <option value="Mechatronics">BEng Machatronics</option>
+            <option value="Mechanical Engineering">BEng Mechanical Engineering</option>
            </select>
         </div>
 
