0fec44f0cbece1c680cd554f920bd0285f5f1479
Lungilemngo <45350946+Lungilemngo@users.noreply.github.com>
2019-09-17 14:27:29 +0200

    updates

--- UCT/uctApplicationsPHP.php
+++ UCT/uctApplicationsPHP.php
@@ -5,7 +5,7 @@ $id = (int)$_SESSION['user_id'];
 $first = $_POST["firstChoice"];
 $second = $_POST["secondChoice"];
 $third = $_POST["ThirdChoice"];
-$mysql_ins="insert into wits_application (COURSE1, COURSE2, COURSE3, USER_ID) values ('$first', '$second', '$third', $id);";
+$mysql_ins="insert into uct_application (COURSE1, COURSE2, COURSE3, USER_ID) values ('$first', '$second', '$third', $id);";
 $result = mysqli_query($conn,$mysql_ins);
 if  ($result)
 {
--- php/LogOut.php
+++ php/LogOut.php
@@ -2,5 +2,5 @@
 session_start();
 unset($_SESSION['user_id']);
 session_destroy();
-header('Location: WelcomePage.html');
+header('Location: ../html/WelcomePage.html');
  ?>
