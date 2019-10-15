eac970f94d9516cb88f76a12c5a82d47d6da0051
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-10-15 14:00:14 +0200

    new descrp

--- UJ/ujBA.php
+++ UJ/ujBA.php
@@ -54,6 +54,42 @@ session_start();
            </select>
 
         </div>
+             <div class="">
+          <h3>BA (DIGITAL  MEDIA DESIGN)</h3>
+          <p>
+Business skills are in high demand worldwide. They develop leaders with knowledge and expertise across a broad range of disciplines that have immediate value in the workplace
+          </p>
+        </div>
+        <div class="">
+          <h3>BA (INTERIOR DESIGN)</h3>
+          <p>
+  Graduates become general managers, tax advisors, internal auditors or business advisors. The CA option is designed for students who intend to qualify as Chartered Accountants.
+          </p>
+        </div>
+        <div class="">
+          <h3>BA (FASHION DESIGN)</h3>
+          <p>
+  This is intended for students with a strong numerate ability who wish to enter the actuarial or allied professions.
+          </p>
+        </div>
+        <div class="">
+          <h3>BA VISUAL ARTS</h3>
+          <p>
+The curriculum deals not only with the information technologies but also imparts students with the skills to analyse user requirements and develop information systems.
+          </p>
+        </div>
+        <div class="">
+          <h3>BA DESIGN (COMMUNICATION DESIGN)</h3>
+          <p>
+ This focuses on financial issues and provides students with the skills needed to operate in the world of finance.
+          </p>
+        </div>
+        <div class="">
+          <h3>BA ARCHITECTURE</h3>
+          <p>
+ This focuses on financial issues and provides students with the skills needed to operate in the world of finance.
+          </p>
+        </div>
 
         <div>
            <input type="submit" name = "submit" class="sumbit" value="SUBMIT">
