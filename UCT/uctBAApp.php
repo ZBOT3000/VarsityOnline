66f7008a3acc5c4fb13f3d1c592ce2afade86d17
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-10-15 13:49:32 +0200

    new description

--- UCT/uctBAApp.php
+++ UCT/uctBAApp.php
@@ -20,15 +20,10 @@ session_start();
   				<select name="firstChoice" class="select-box">
     				<option>Select your first Choice</option>
             <option value="Fine Arts">BA Fine Arts</option>
-            <option>The degree is designed to enable students to develop as artists at a level of best practice.</option>
             <option value="Theatre & Performance">BA Theatre & Performance</option>
-            <option>These qualifications are intended for those who wish to work professionally in the theatre as performers, makers, designers, teachers, and facilitators or as academics and researchers.</option>
             <option value="Social Science">BA Social Science</option>
-            <option>Responsible for studying and researching the link between society and human behavior. Specializes in any of the natural sciences, including anthropology, archaeology, criminology, economics.</option>
             <option value="Social Work">BA Social Work</option>
-            <option>Social Workers interact with individuals, families, groups and communities in life-skills training, adult education, economic empowerment and various prevention programmes.</option>
             <option value="Music">BA Music</option>
-            <option>The School offers training in the entire range of orchestral instruments, piano, organ, voice, African percussion and jazz. </option>
   		     </select>
 
   			</div>
@@ -53,6 +48,37 @@ session_start();
             <option value="Social Work">BA Social Work</option>
             <option value="Music">BA Music</option>
            </select>
+        </div>
+
+        <div class="">
+          <h3>Fine Arts</h3>
+          <p>
+The degree is designed to enable students to develop as artists at a level of best practice.
+          </p>
+        </div>
+        <div class="">
+          <h3>Theatre & Performance</h3>
+          <p>
+  These qualifications are intended for those who wish to work professionally in the theatre as performers, makers, designers, teachers, and facilitators or as academics and researchers.
+          </p>
+        </div>
+        <div class="">
+          <h3>Social Science</h3>
+          <p>
+  Responsible for studying and researching the link between society and human behavior. Specializes in any of the natural sciences, including anthropology, archaeology, criminology, economics.
+          </p>
+        </div>
+        <div class="">
+          <h3>Social Work</h3>
+          <p>
+Social Workers interact with individuals, families, groups and communities in life-skills training, adult education, economic empowerment and various prevention programmes
+          </p>
+        </div>
+        <div class="">
+          <h3>Music</h3>
+          <p>
+The School offers training in the entire range of orchestral instruments, piano, organ, voice, African percussion and jazz.
+          </p>
         </div>
 
         <div>
