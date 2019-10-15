a83aa8cb277d98f0961666068f8a077926205430
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-10-15 13:12:47 +0200

    Course descriptions

--- UCT/uctBEngApp.php
+++ UCT/uctBEngApp.php
@@ -22,13 +22,13 @@ session_start();
             <option value="Civil Engineering">BEng Civil Engineering</option>
 	    <option>Civil engineers create, improve and protect the environment in which we live. They plan, design and oversee construction and maintenance of building structures and infrastructure.</option>
             <option value="Electrical Engineering">BEng Electrical Engineering</option>
-            <option>Electrical engineers design, develop, and test electrical devices and equipment, including communications systems, power generators, motors and navigation systems</option>
+            <option>The Electrical Engineering degree curriculum at UCT has been designed to be flexible. Electrical engineers can choose to follow a high technology career in research and development.</option>
             <option value="Chemical Engineering">BEng Chemical Engineering</option>
             <option>Chemical engineers apply the principles of chemistry, biology, physics, and math to solve problems that involve the use of fuel, drugs, food, and many other products.</option>
             <option value="Mechatronics">BEng Machatronics</option>
             <option>Design, develop, maintain and manage high technology engineering systems for the automation of industrial tasks.</option>
             <option value="Mechanical Engineering">BEng Mechanical Engineering</option>
-	    <option>Mechanical engineers design power-producing machines such as electric generators, internal combustion engines, and steam and gas turbines as well as power-using machines.</option>
+	    <option>Mechanical engineers design power-producing machines such as electric generators, involves understanding and harnessing forces and energy in machines for the benefit of society</option>
   		     </select>
 
   			</div>
