0ea5073c45b73bed5267382d5f073e605252fb88
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-10-15 14:02:57 +0200

    new descrip

--- UJ/ujCommerce.php
+++ UJ/ujCommerce.php
@@ -78,6 +78,36 @@ session_start();
            </select>
 
         </div>
+             <div class="">
+          <h3>BUSINESS MANAGEMENT</h3>
+          <p>
+Business skills are in high demand worldwide. They develop leaders with knowledge and expertise across a broad range of disciplines that have immediate value in the workplace
+          </p>
+        </div>
+        <div class="">
+          <h3>ACCOUNTING</h3>
+          <p>
+  Graduates become general managers, tax advisors, internal auditors or business advisors. The CA option is designed for students who intend to qualify as Chartered Accountants.
+          </p>
+        </div>
+        <div class="">
+          <h3>FINANCE</h3>
+          <p>
+  This is intended for students with a strong numerate ability who wish to enter the finance or allied professions.
+          </p>
+        </div>
+        <div class="">
+          <h3>INFORMATION MANAGEMEN</h3>
+          <p>
+The curriculum deals not only with the information technologies but also imparts students with the skills to analyse user requirements and develop information systems.
+          </p>
+        </div>
+        <div class="">
+          <h3>ECONOMICS AND ECONOMETRICS</h3>
+          <p>
+ This focuses on financial issues and provides students with the skills needed to operate in the world of finance.
+          </p>
+        </div>
 
         <div>
            <input type="submit" name = "submit" class="sumbit" value="SUBMIT">
