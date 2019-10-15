25b03a838d679e948445cfb64251e9c1e8db4772
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-10-15 13:53:48 +0200

    new description

--- UCT/uctComApp.php
+++ UCT/uctComApp.php
@@ -20,15 +20,10 @@ session_start();
   				<select name="firstChoice" class="select-box">
     				<option>Select your first Choice</option>
             <option value="Business Science">Bcom Business Science</option>
-	    <option> Business skills are in high demand worldwide. They develop leaders with knowledge and expertise across a broad range of disciplines that have immediate value in the workplace</option>
             <option value="Accounting">Bcom Accounting</option>
-	    <option> Graduates become general managers, tax advisors, internal auditors or business advisors. The CA option is designed for students who intend to qualify as Chartered Accountants.</option>
-            <option value="Actuarial Science">Bcom Actuarial Science</option>
-	    <option> This is intended for students with a strong numerate ability who wish to enter the actuarial or allied professions. </option>
+            <option value="Actuarial Science">Bcom Actuarial Science</option
             <option value="Information Systems">Bcom Information Systems</option>
-            <option> The curriculum deals not only with the information technologies but also imparts students with the skills to analyse user requirements and develop information systems.</option>
             <option value="Economics">Bcom Economics</option>
-            <option> This focuses on financial issues and provides students with the skills needed to operate in the world of finance.</option>
   		     </select>
 
   			</div>
@@ -53,6 +48,37 @@ session_start();
             <option value="Information Systems">Bcom Information Systems</option>
             <option value="Economics">Bcom Economics</option>
   		     </select>
+        </div>
+
+        <div class="">
+          <h3>Business Science</h3>
+          <p>
+Business skills are in high demand worldwide. They develop leaders with knowledge and expertise across a broad range of disciplines that have immediate value in the workplace
+          </p>
+        </div>
+        <div class="">
+          <h3>Accounting</h3>
+          <p>
+  Graduates become general managers, tax advisors, internal auditors or business advisors. The CA option is designed for students who intend to qualify as Chartered Accountants.
+          </p>
+        </div>
+        <div class="">
+          <h3>Actuarial Science</h3>
+          <p>
+  This is intended for students with a strong numerate ability who wish to enter the actuarial or allied professions.
+          </p>
+        </div>
+        <div class="">
+          <h3>Information Systems</h3>
+          <p>
+The curriculum deals not only with the information technologies but also imparts students with the skills to analyse user requirements and develop information systems.
+          </p>
+        </div>
+        <div class="">
+          <h3>Economics</h3>
+          <p>
+ This focuses on financial issues and provides students with the skills needed to operate in the world of finance.
+          </p>
         </div>
 
         <div>
