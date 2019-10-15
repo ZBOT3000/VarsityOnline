662a3729afc8d5cddd9551ef5bd2b59998ae8524
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-10-15 14:12:22 +0200

    new descrip

--- WITS/witsBAApp.php
+++ WITS/witsBAApp.php
@@ -56,6 +56,36 @@ session_start();
             <option value="BA-Music">BA-Music</option>
            </select>
         </div>
+     <div class="">
+          <h3>Fine Arts</h3>
+          <p>
+The degree is designed to enable students to develop as artists at a level of best practice.
+          </p>
+        </div>
+        <div class="">
+          <h3>Dramatic Arts"</h3>
+          <p>
+  These qualifications are intended for those who wish to work professionally in the theatre as performers, makers, designers, teachers, and facilitators or as academics and researchers.
+          </p>
+        </div>
+        <div class="">
+          <h3>Social Science</h3>
+          <p>
+  Responsible for studying and researching the link between society and human behavior. Specializes in any of the natural sciences, including anthropology, archaeology, criminology, economics.
+          </p>
+        </div>
+        <div class="">
+          <h3>Social Work</h3>
+          <p>
+Social Workers interact with individuals, families, groups and communities in life-skills training, adult education, economic empowerment and various prevention programmes
+          </p>
+        </div>
+        <div class="">
+          <h3>Music</h3>
+          <p>
+The School offers training in the entire range of orchestral instruments, piano, organ, voice, African percussion and jazz.
+          </p>
+        </div>
 
         <div>
            <input type="submit" name = "submit" class="sumbit" value="SUBMIT">
