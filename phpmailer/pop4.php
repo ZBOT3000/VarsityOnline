3cf459ac6f85a9aae62482f5b4c11597a286f2bc
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-11-10 16:29:40 +0000

    updated

--- phpmailer/pop4.php
+++ phpmailer/pop4.php
@@ -15,5 +15,27 @@ function testings($a,$b){
 		return $b;
 }
 
+function mark($mark)
+{
+	$total =0 ;
+	if ($mark >= 80 and $mark <101)
+	{
+		$total = 7 ;
+	}else if ($mark >= 70 and $mark < 80)
+	{
+		$total = 6 ;
+	}else if ($mark >=60 and $mark < 70)
+	{
+		$total = 5 ;
+	}else if ($mark >= 50 and $mark < 60)
+	{
+		$total = 4 ;
+	}else if ($mark >= 40 and $mark < 50)
+	{
+		$total = 3 ;
+	}
+	return $total;
+	}
+
 
  ?>
