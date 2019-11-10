1d1b3ab98ad42fda3fcd08381611b40786a02251
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-11-10 16:07:47 +0000

    coverall marks

---
+++ phpmailer/unitTests/test_marks.php
@@ -0,0 +1,14 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
+#require_once('locallib.php');
+class markTest extends TestCase{
+  
+ public function test_mark(){
+   require_once('/home/travis/build/noreplysims/VarsityOnline/phpmailer/UJAPS.php');
+   $test=mark(71);
+ $this->assertEquals(6,$test, "correct!"); 
+ }
+ 
+}
+?>
