b3e7a3eeaf3b2c3fee8210ecba685fc253a992b5
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-11-10 20:59:25 +0000

    Update test_marksWits.php

--- phpmailer/unitTests/test_marksWits.php
+++ phpmailer/unitTests/test_marksWits.php
@@ -8,6 +8,10 @@ class markWitsTest extends TestCase{
    require_once('/home/travis/build/noreplysims/VarsityOnline/phpmailer/WITSAPS.php');
    $tests=APS(45);
  $this->assertEquals(3,$tests, "correct!"); 
+   $tests2=APS_LO(45);
+ $this->assertEquals(3,$tests2, "correct!");
+   $tests3=APS_EM(45);
+ $this->assertEquals(3,$tests3, "correct!");
  }
  
 }
