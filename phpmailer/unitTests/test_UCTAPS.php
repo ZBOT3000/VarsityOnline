de959cbf00d63237930762ce02641bb9f1857d6b
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-11-10 21:44:37 +0000

    Update test_UCTAPS.php

--- phpmailer/unitTests/test_UCTAPS.php
+++ phpmailer/unitTests/test_UCTAPS.php
@@ -8,6 +8,8 @@ class UCTAPSTest extends TestCase{
    require_once('/home/travis/build/noreplysims/VarsityOnline/phpmailer/UCTAPS.php');
    $tests=UCT_APS(79);
  $this->assertEquals(6,$tests, "correct!"); 
+   $tests2=UCT_APS(47);
+ $this->assertEquals(3,$tests2, "correct!"); 
  }
  
 }
