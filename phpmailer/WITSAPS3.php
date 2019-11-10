6698c30ffc9b9c017f83ccaec5d070c513e6cb78
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-11-10 21:09:35 +0000

    Create WITSAPS3.php

---
+++ phpmailer/WITSAPS3.php
@@ -0,0 +1,27 @@
+<? php
+
+function APS_EM($mark) {
+	$total = 0;
+	if ($mark >= 90)
+	{
+		$total = 10 ;
+	} else if ($mark >= 80 and $mark < 90)
+	{
+		$total = 9 ;
+	}else if ($mark >= 70 and $mark < 80)
+	{
+		$total = 8 ;
+	}else if ($mark >=60 and $mark < 70)
+	{
+		$total = 7 ;
+	}else if ($mark >= 50 and $mark < 60)
+	{
+		$total = 4 ;
+	}else if ($mark >= 40 and $mark < 50)
+	{
+		$total = 3 ;
+	}
+return $total;
+}
+
+?>
