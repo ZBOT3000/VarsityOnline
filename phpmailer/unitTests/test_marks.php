6e00f615ba2c4ce1acd9dcafaf66adaaaa4d7f36
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-11-10 16:32:02 +0000

    Update test_marks.php

--- phpmailer/unitTests/test_marks.php
+++ phpmailer/unitTests/test_marks.php
@@ -4,7 +4,7 @@ use PHPUnit\DbUnit\TestCaseTrait;
 #require_once('locallib.php');
 class markTest extends TestCase{
   
- public function test_mark(){
+ public function test_testing(){
    require_once('/home/travis/build/noreplysims/VarsityOnline/phpmailer/UJAPS.php');
    $test=mark(71);
  $this->assertEquals(6,$test, "correct!"); 
