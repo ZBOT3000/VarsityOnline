6bc30342fe5b7987b807b177755a0a6c2f57ed95
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-11-10 21:35:24 +0000

    Update test_UCTAPS.php

--- phpmailer/unitTests/test_UCTAPS.php
+++ phpmailer/unitTests/test_UCTAPS.php
@@ -2,7 +2,7 @@
 use PHPUnit\Framework\TestCase;
 use PHPUnit\DbUnit\TestCaseTrait;
 #require_once('locallib.php');
-class markWitsTest extends TestCase{
+class markUCTTest extends TestCase{
   
  public function test_testing(){
    require_once('/home/travis/build/noreplysims/VarsityOnline/phpmailer/WITSAPS.php');
