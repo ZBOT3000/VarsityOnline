0fa163c9fa5fc11f293da58510ff55d1f1c523fd
ZBOT3000 <30493727+ZBOT3000@users.noreply.github.com>
2019-11-10 16:34:35 +0000

    updated

--- phpmailer/unitTests/test_marks.php
+++ phpmailer/unitTests/test_marks.php
@@ -6,8 +6,8 @@ class markTest extends TestCase{
   
  public function test_testing(){
    require_once('/home/travis/build/noreplysims/VarsityOnline/phpmailer/UJAPS.php');
-   $test=mark(71);
- $this->assertEquals(6,$test, "correct!"); 
+   $tests=mark(71);
+ $this->assertEquals(6,$tests, "correct!"); 
  }
  
 }
